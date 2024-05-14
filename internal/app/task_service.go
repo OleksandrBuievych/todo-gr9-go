089862a454d242e71a98da959cdea66c7434416d
BohdanBoriak <lightraven72@gmail.com>
2024-05-14 11:13:25 +0300

    Added: Save method to TaskController

--- internal/app/task_service.go
+++ internal/app/task_service.go
@@ -1,8 +1,14 @@
 package app
 
-import "github.com/BohdanBoriak/boilerplate-go-back/internal/infra/database"
+import (
+	"log"
+
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/database"
+)
 
 type TaskService interface {
+	Save(t domain.Task) (domain.Task, error)
 }
 
 type taskService struct {
@@ -13,4 +19,13 @@ func NewTaskService(tr database.TaskRepository) TaskService {
 	return taskService{
 		taskRepo: tr,
 	}
+}
+
+func (s taskService) Save(t domain.Task) (domain.Task, error) {
+	task, err := s.taskRepo.Save(t)
+	if err != nil {
+		log.Printf("TaskService -> Save: %s", err)
+		return domain.Task{}, err
+	}
+	return task, nil
 }
